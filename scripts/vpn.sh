5ce0e86e3c23356292f0df08c479090c1d7afbfb
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-04 20:49:38 +0100

    Create vpn.sh

---
+++ scripts/vpn.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ==============================================================================
+#
+# This script checks the status of my VPN connection, and makes it possible
+# to toggle on and off using polybar.
+#
+# It is more or less just a slimmed down and slightly modified version of 
+# vpn_module.sh by Shervin S. (shervin@tuta.io):
+#
+# https://github.com/shervinsahba/polybar-vpn-controller
+#
+# ==============================================================================
+
+vpn_status=$(mullvad status | cut -d' ' -f3)
+ip_address=$(mullvad status | awk 'match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/){print substr($0,RSTART,RLENGTH)}')
+protocol=$(mullvad status | awk '{for(i=1;i<=NF;i++)if($i=="to")print $(i+1)}')
+
+vpn_report() {
+	if [ "$vpn_status" = "Connected" ]; then
+		echo "%{F#CCCCCC}$ip_address ($protocol)%{F-}"
+	elif [ "$vpn_status" = "Connecting" ]; then
+		echo "%{F#CCCCCC}connecting...%{F-}"
+	else
+		echo "%{F#FBB86C}disconnected%{F-}"
+	fi
+}
+
+vpn_toggle_connection() {
+  if [ "$vpn_status" = "Connected" ]; then
+      mullvad disconnect
+  else
+      mullvad connect
+  fi
+}
+
+case "$1" in
+	--toggle-connection) vpn_toggle_connection ;;
+	*) vpn_report ;;
+esac
