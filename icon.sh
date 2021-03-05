aa82ada563cbe56b10291673a40d6a3d0b1ca8fc
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-05 20:05:07 +0100

    Delete icon.sh

--- icon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo sh -c "echo 'deb http://ppa.launchpad.net/papirus/papirus/ubuntu focal main' > /etc/apt/sources.list.d/papirus-ppa.list"
-
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com E58A9D36647CAE7F
-sudo apt update
-sudo apt install -y papirus-icon-theme
