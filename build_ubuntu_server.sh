15bc8f233aab0be457757ae7743b01730bb63bf6
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-07 10:26:27 +0100

    Create build_ubuntu_server.sh

---
+++ build_ubuntu_server.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+# ==============================================================================
+#
+# This script gets my workstation up and running after a clean install,
+# INCLUDING setting up UFW.
+#
+# This might not be what you want! Make sure to read through this file and make
+# changes accordingly!
+#
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Define a try catch function
+# Based on: https://medium.com/@dirk.avery/the-bash-trap-trap-ce6083f36700
+# ------------------------------------------------------------------------------
+
+set -e
+trap 'catch $? $LINENO' EXIT
+
+catch() {
+  if [ "$1" != "0" ]; then
+    echo -e "\033[1;31mInstallation failed!\033[0m"
+    echo -e "\033[1;31mError $1 occurred on $2\033[0m"
+  fi
+}
+
+simple() {
+  
+  # ------------------------------------------------------------------------------
+  # Install required packages
+  # ------------------------------------------------------------------------------
+
+  sudo apt install -y \
+  xorg bspwm kitty polybar suckless-tools rofi pass \
+  build-essential apt-transport-https  \
+  alsa-utils libavcodec-extra \
+  zathura ranger nautilus feh syncthing neofetch firefox
+  
+  # passmenu
+  sudo cp /usr/share/doc/pass/examples/dmenu/passmenu /usr/bin/passmenu
+  sudo chmod +x /usr/bin/passmenu
+  
+  # rofi-pass
+  cd /tmp
+  wget https://github.com/carnager/rofi-pass/archive/master.zip
+  unzip master.zip
+  rm master.zip
+  cd rofi-pass-master
+  sudo make
+  mkdir -p ~/.config/rofi-pass
+  mv config.example ~/.config/rofi-pass/config
+  cd ..
+  sudo rm -rf rofi-pass-master
+  cd
+  
+  # rofi-power-menu
+  cd /tmp
+  wget https://raw.githubusercontent.com/jluttine/rofi-power-menu/master/rofi-power-menu
+  sudo mv rofi-power-menu /usr/local/bin
+  sudo chmod +x /usr/local/bin/rofi-power-menu
+  cd
+
+  # ------------------------------------------------------------------------------
+  # Fonts 
+  # ------------------------------------------------------------------------------
+  
+  # google fonts
+  cd /tmp
+  wget -O fonts.zip "https://fonts.google.com/download?family=Roboto|Noto%20Sans|Open%20Sans|Roboto%20Condensed|Source%20Sans%20Pro|Raleway|Merriweather|Roboto%20Slab|PT%20Sans|Open%20Sans%20Condensed|Droid%20Sans|Droid%20Serif|Fira%20Sans|Fira%20Sans%20Condensed|Fira%20Sans%20Extra%20Condensed|Fira%20Mono"
+  unzip fonts.zip -d ~/.local/share/fonts
+  rm -rf fonts.zip
+  cd
+  
+  # fira code
+  mkdir -p ~/.local/share/fonts
+  cd ~/.local/share/fonts
+  wget https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip
+  unzip *.zip
+  mv ~/.local/share/fonts/ttf/*.ttf ~/.local/share/fonts
+  rm *.txt
+  rm *.html
+  rm *.zip
+  rm *.css
+  rm -rf woff
+  rm -rf woff2
+  rm -rf variable_ttf
+  rm -rf ttf
+  
+  # fontawesome
+  wget https://use.fontawesome.com/releases/v5.15.2/fontawesome-free-5.15.2-desktop.zip
+  unzip *.zip
+  mv fontawesome-free-5.15.2-desktop/otfs/*.otf ~/.local/share/fonts/
+  rm *.zip
+  rm -rf fontawesome-free-5.15.2-desktop
+  cd
+  
+  fc-cache -f -v
+  
+  # ------------------------------------------------------------------------------
+  # Papirus icons 
+  # ------------------------------------------------------------------------------
+  
+  sudo sh -c "echo 'deb http://ppa.launchpad.net/papirus/papirus/ubuntu focal main' > /etc/apt/sources.list.d/papirus-ppa.list"
+  sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com E58A9D36647CAE7F
+  sudo apt update
+  sudo apt install -y papirus-icon-theme
+  
+  # ------------------------------------------------------------------------------
+  # Make sure relevant configs and scripts are executable
+  # ------------------------------------------------------------------------------
+  
+  chmod +x ~/dotfiles/config/bspwm/bspwmrc
+  chmod +x ~/dotfiles/scripts/launch.sh
+  chmod +x ~/dotfiles/scripts/vpn.sh
+  chmod +x ~/dotfiles/scripts/upgrades.sh
+  chmod +x ~/dotfiles/scripts/backup.sh
+  chmod +x ~/dotfiles/scripts/weather.R
+
+  # ------------------------------------------------------------------------------
+  # Set up symlinks
+  # ------------------------------------------------------------------------------
+  
+  mkdir -p ~/.config/{bspwm,sxhkd,kitty,rofi,rofi-pass,ranger,zathura}
+  ranger --copy-config=all
+
+  ln -s -f ~/dotfiles/config/.bashrc ~/.bashrc
+  #ln -s -f ~/dotfiles/config/.xinitrc ~/.xinitrc
+  ln -s -f ~/dotfiles/config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+  ln -s -f ~/dotfiles/config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+  ln -s -f ~/dotfiles/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+  ln -s -f ~/dotfiles/config/rofi/my_theme.rasi ~/.config/rofi/my_theme.rasi
+  ln -s -f ~/dotfiles/config/rofi-pass/config ~/.config/rofi-pass/config
+  ln -s -f ~/dotfiles/config/ranger/rifle.conf ~/.config/ranger/rifle.conf
+  ln -s -f ~/dotfiles/config/ranger/rc.conf ~/.config/ranger/rc.conf
+  ln -s -f ~/dotfiles/config/zathura/zathurarc ~/.config/zathura/zathurarc
+  
+  # ------------------------------------------------------------------------------
+  # Clean up
+  # ------------------------------------------------------------------------------
+  
+  #sudo apt autoremove -y
+}
+
+simple
+echo ""
+echo -e "\033[1;32mEverything is set up, time to reboot!\033[0m"
+echo ""
