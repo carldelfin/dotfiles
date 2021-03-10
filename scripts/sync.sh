68e514f33c8618799f2c5f421ffc9ec8c5e6357b
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-10 21:14:44 +0100

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-
 # ==============================================================================
 #
 # Simple script that checks if Syncthing is running by looking for the GUI IP.
+# Left clicking (in Polybar) opens the GUI in Firefox.
 #
 # Make sure to add your own API key!
 #
 # ==============================================================================
 
-gui_ip=$(curl -s -X GET -H "X-API-Key: <API key here>" http://localhost:8384/rest/system/status | jq --raw-output '.guiAddressUsed')
+gui_ip=$(curl -s -X GET -H "X-API-Key: qonjDjLwKhpgUhSVXepSsnMGh93oWRJm" http://localhost:8384/rest/system/status | jq --raw-output '.guiAddressUsed')
 
-if !($gui_ip > /dev/null 2>&1); then
+check_status() {
+	if !($gui_ip > /dev/null 2>&1); then
     echo $gui_ip
-else
-    echo "%{F#FBB86C}syncthing not running%{F-}"
-fi
+  else
+      echo "%{F#FBB86C}syncthing not running%{F-}"
+  fi
+}
+
+open_browser() {
+  firefox http://localhost:8384
+}
+
+case "$1" in
+	--open-browser) open_browser ;;
+	*) check_status ;;
+esac
