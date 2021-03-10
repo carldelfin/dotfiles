cc9a7c39fee471776164197c8815697411f0595b
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-10 13:27:18 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ simple() {
 
   sudo apt install -y \
   bspwm kitty polybar suckless-tools rofi pass \
-  apt-transport-https lxappearance \
+  apt-transport-https lxappearance libavcodec-extra \
   zathura ranger feh htop syncthing neofetch
 
   # passmenu
