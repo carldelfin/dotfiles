f6b3e07924cb10e0cb0904a3b6a167c0c17afeb6
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-10 21:22:05 +0100

    Update install_vscode.sh

--- install/install_vscode.sh
+++ install/install_vscode.sh
@@ -19,11 +19,7 @@ catch() {
 }
 
 simple() {
-  cd /tmp
-  wget https://az764295.vo.msecnd.net/stable/ea3859d4ba2f3e577a159bc91e3074c5d85c0523/code_1.52.1-1608136922_amd64.deb
-  sudo apt install -y ./*.deb
-  rm *.deb
-  cd
+  sudo apt install -y code
 }
 
 simple
