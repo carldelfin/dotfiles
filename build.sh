641cc364ca8b0ca1d45447cbd9c58ac67d741e2b
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-10 14:47:40 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -108,6 +108,12 @@ simple() {
   #rm *.deb
   #cd
   
+  # kvm
+  #sudo apt install -y \
+  #qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+  #sudo adduser `id -un` libvirt
+  #sudo adduser `id -un` kvm
+  
   # ------------------------------------------------------------------------------
   # Remove redundant packages 
   # ------------------------------------------------------------------------------
