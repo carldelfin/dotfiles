216ee5751371472d714576ebce8610bde218be1a
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-10 21:20:24 +0100

    Update install_r_rstudio.sh

--- install/install_r_rstudio.sh
+++ install/install_r_rstudio.sh
@@ -21,21 +21,22 @@ catch() {
 
 simple() {
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-  echo 'deb https://cloud.r-project.org/bin/linux/ubuntu groovy-cran40/' | sudo tee -a /etc/apt/sources.list
+  sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu groovy-cran40/'
   sudo apt update
+  
   sudo apt install -y \
   r-base r-base-dev \
-  libclang-dev libssl-dev libxml2-dev \
-  libcurl4-openssl-dev libssl-dev libxt-dev \
+  libclang-dev libssl-dev \
+  libcurl4-openssl-dev libxt-dev \
   libopenblas-dev liblapack-dev libopencv-dev \
   libcairo2-dev libnode-dev
-  
+
   cd /tmp
   wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1103-amd64.deb
   sudo apt install -y ./*.deb
   rm *.deb
   cd
-}
+ }
 
 simple
 echo -e "\033[1;32mInstallation OK\033[0m"
