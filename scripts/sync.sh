b2ae924849f5470b3c503d22af0c8903e23cff64
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-11 06:51:45 +0100

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -9,13 +9,13 @@
 #
 # ==============================================================================
 
-gui_ip=$(curl -s -X GET -H "X-API-Key: qonjDjLwKhpgUhSVXepSsnMGh93oWRJm" http://localhost:8384/rest/system/status | jq --raw-output '.guiAddressUsed')
+gui_ip=$(curl -s -X GET -H "X-API-Key: <API key here>" http://localhost:8384/rest/system/status | jq --raw-output '.guiAddressUsed')
 
 check_status() {
   if [ "$gui_ip" = "127.0.0.1:8384" ]; then
     echo $gui_ip
   else
-    echo "%{F#FBB86C}syncthing not running%{F-}"
+    echo "%{F#FBB86C}no sync%{F-}"
   fi
 }
 
