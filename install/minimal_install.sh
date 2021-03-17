493eec15170f85faacc68f711c5bdf5834ec44e9
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-17 08:45:24 +0100

    Update minimal_install.sh

--- install/minimal_install.sh
+++ install/minimal_install.sh
@@ -37,8 +37,12 @@ simple() {
   jq neovim qt5-style-plugins
   
   # vim-plug
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  if [ -d "$HOME/.vim/plugged" ]; then
+      echo "vim-plug already installed"
+  else
+      sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  fi
   
   # passmenu
   if ! command -v passmenu &> /dev/null; then
