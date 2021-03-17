9f524c3890be476733aa1ef2afa7b29d304fdee1
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-17 07:18:56 +0100

    Update system_setup.sh

--- install/system_setup.sh
+++ install/system_setup.sh
@@ -36,6 +36,10 @@ simple() {
   okular ranger feh htop syncthing neofetch arandr \
   jq neovim python3-pip qt5-style-plugins
   
+  # vim-plug
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  
   # passmenu
   sudo cp /usr/share/doc/pass/examples/dmenu/passmenu /usr/bin/passmenu
   sudo chmod +x /usr/bin/passmenu
