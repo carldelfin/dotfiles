7ac71b2dec7dc342b6ff0ceef651a7d79cc6a141
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-17 07:35:54 +0100

    Update system_setup.sh

--- install/system_setup.sh
+++ install/system_setup.sh
@@ -140,6 +140,7 @@ simple() {
   ranger --copy-config=all
 
   ln -s -f ~/dotfiles/config/.bashrc ~/.bashrc
+  ln -s -f ~/dotfiles/config/.radian_profile ~/.radian_profile
   ln -s -f ~/dotfiles/config/mimeapps.list ~/.config/mimeapps.list
   ln -s -f ~/dotfiles/config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
   ln -s -f ~/dotfiles/config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
