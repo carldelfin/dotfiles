58f9f7c5fc92e760ae2514c2e9c6946b6555c454
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-17 20:23:03 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,81 +1,167 @@
 #!/bin/bash
 
-# ==============================================================================
+# ==================================================================================================
 #
-# This script makes it possible to select what to install from my dotfiles
+#			 _               _   _       _             _     
+#			| |             (_) | |     | |           | |    
+#			| |__    _   _   _  | |   __| |      ___  | |__  
+#			| '_ \  | | | | | | | |  / _` |     / __| | '_ \ 
+#			| |_) | | |_| | | | | | | (_| |  _  \__ \ | | | |
+#			|_.__/   \__,_| |_| |_|  \__,_| (_) |___/ |_| |_|
+# 
 #
-# It's based on work by dols3m and sergiofbsilva from
-# https://stackoverflow.com/a/54261882/317605
-# and
-# https://stackoverflow.com/users/8207842/dols3m
+# This scripts lets you select what applications to install, including and beyond the intitial
+# system setup. I use more or less all of them, but sometimes only have use for some of them on
+# certain machines. In addition, keeping them separate is quite handy for testing purposes.
+# 
+# The script is based on previous work by Nathan Davieau, MestreLion, dols3m, and sergiofbsilva:
 #
-# ==============================================================================
-
+# https://serverfault.com/a/777849
+# https://stackoverflow.com/a/54261882
+# https://gist.github.com/sergiofbsilva/099172ea597657b0d0008dc367946953
+#
+# ==================================================================================================
 
 # define colors
 GREEN='\033[1;32m'
-RED='\033[0;31m'
 NC='\033[0m'
 
-# define menu options
-options[0]="${RED}Abort${NC}"
-options[1]="System setup"
-options[2]="Install R and Radian"
-options[4]="Install Zotero"
-options[5]="Install TexLive"
-options[6]="Install Zoom"
+function prompt_for_multiselect {
+
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()   { printf "$ESC[?25h"; }
+    cursor_blink_off()  { printf "$ESC[?25l"; }
+    cursor_to()         { printf "$ESC[$1;${2:-1}H"; }
+    print_inactive()    { printf "$2   $1 "; }
+    print_active()      { printf "$2  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()    { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    key_input()         {
+      local key
+      IFS= read -rsn1 key 2>/dev/null >&2
+      if [[ $key = ""      ]]; then echo enter; fi;
+      if [[ $key = $'\x20' ]]; then echo space; fi;
+      if [[ $key = $'\x1b' ]]; then
+        read -rsn2 key
+        if [[ $key = [A ]]; then echo up;    fi;
+        if [[ $key = [B ]]; then echo down;  fi;
+      fi 
+    }
+    toggle_option()    {
+      local arr_name=$1
+      eval "local arr=(\"\${${arr_name}[@]}\")"
+      local option=$2
+      if [[ ${arr[option]} == true ]]; then
+        arr[option]=
+      else
+        arr[option]=true
+      fi
+      eval $arr_name='("${arr[@]}")'
+    }
+
+    local retval=$1
+    local options
+    local defaults
+
+    IFS=';' read -r -a options <<< "$2"
+    if [[ -z $3 ]]; then
+      defaults=()
+    else
+      IFS=';' read -r -a defaults <<< "$3"
+    fi
+    local selected=()
+
+    for ((i=0; i<${#options[@]}; i++)); do
+      selected+=("${defaults[i]:-false}")
+      printf "\n"
+    done
+
+    # determine current screen position for overwriting the options
+    local lastrow=`get_cursor_row`
+    local startrow=$(($lastrow - ${#options[@]}))
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local active=0
+    while true; do
+        # print options by overwriting the last lines
+        local idx=0
+        for option in "${options[@]}"; do
+            local prefix="[ ]"
+            if [[ ${selected[idx]} == true ]]; then
+              prefix="[${GREEN}x${NC}]"
+            fi
+
+            cursor_to $(($startrow + $idx))
+            if [ $idx -eq $active ]; then
+                print_active "$option" "$prefix"
+            else
+                print_inactive "$option" "$prefix"
+            fi
+            ((idx++))
+        done
+
+        # user key control
+        case `key_input` in
+            space)  toggle_option selected $active;;
+            enter)  break;;
+            up)     ((active--));
+                    if [ $active -lt 0 ]; then active=$((${#options[@]} - 1)); fi;;
+            down)   ((active++));
+                    if [ $active -ge ${#options[@]} ]; then active=0; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    eval $retval='("${selected[@]}")'
+}
+
+# possible options
+OPTIONS_VALUES=("Exit" "Initial system setup" "Install R and Radian" "Install R packages" "Install Zotero" "Install TexLive" "Install Zoom")
+
+for i in "${!OPTIONS_VALUES[@]}"; do
+	OPTIONS_STRING+="${OPTIONS_VALUES[$i]};"
+done
 
 # define actions based on options
 function ACTIONS {
-    if [[ ${choices[0]} ]]; then
-        exit 1
+    if [ "${SELECTED[0]}" == "true" ]; then
+    	exit 1
     fi
-    if [[ ${choices[1]} ]]; then
+    if [ "${SELECTED[1]}" == "true" ]; then
         source ~/dotfiles/build/system_setup.sh
     fi
-    if [[ ${choices[2]} ]]; then
+    if [ "${SELECTED[2]}" == "true" ]; then
         source ~/dotfiles/build/install_r.sh
     fi
-    if [[ ${choices[4]} ]]; then
-        source ~/dotfiles/build/04_install_zotero.sh
+    if [ "${SELECTED[3]}" == "true" ]; then
+        source ~/dotfiles/build/install_r_packages.sh
     fi
-    if [[ ${choices[5]} ]]; then
-        source ~/dotfiles/build/05_install_texlive.sh
+    if [ "${SELECTED[4]}" == "true" ]; then
+        source ~/dotfiles/build/install_zotero.sh
     fi
-    if [[ ${choices[6]} ]]; then
-        source ~/dotfiles/build/06_install_zoom.sh
+    if [ "${SELECTED[5]}" == "true" ]; then
+        source ~/dotfiles/build/install_texlive.sh
+    fi
+    if [ "${SELECTED[6]}" == "true" ]; then
+        source ~/dotfiles/build/install_zoom.sh
     fi
 }
-
-ERROR=" "
 
 clear
 
-# menu function
-function MENU {
-    echo ""
-    echo "---------------------------------------------------------------------"
-    for NUM in ${!options[@]}; do
-        echo -e $(( NUM+1 ))" "[""${GREEN}${choices[NUM]:- }"${NC}"]" ${options[NUM]}"
-    done
-    echo "---------------------------------------------------------------------"
-    echo "$ERROR"
-}
+echo ""
+echo "------------------------------------------------------------------------------------------"
+echo "Navigate using arrows, select using SPACE (press again to deselect), press ENTER when done"
+echo "------------------------------------------------------------------------------------------"
+echo ""
 
-# menu loop
-while MENU && read -e -p "Select by pressing the corresponding number (press again to deselect), then ENTER when done: " -n2 SELECTION && [[ -n "$SELECTION" ]]; do
-    clear
-    if [[ "$SELECTION" == *[[:digit:]]* && $SELECTION -ge 1 && $SELECTION -le ${#options[@]} ]]; then
-        (( SELECTION-- ))
-        if [[ "${choices[SELECTION]}" == "+" ]]; then
-            choices[SELECTION]=""
-        else
-            choices[SELECTION]="+"
-        fi
-            ERROR=" "
-    else
-        ERROR="Invalid option: $SELECTION"
-    fi
-done
+prompt_for_multiselect SELECTED "$OPTIONS_STRING"
 
 ACTIONS
