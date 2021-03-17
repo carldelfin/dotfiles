a67e8523b78569c2ca365c484cb6bbf77c7e7b4f
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-17 16:12:46 +0100

    Update 02_system_setup.sh

--- build/02_system_setup.sh
+++ build/02_system_setup.sh
@@ -27,12 +27,29 @@ simple() {
 
   sudo apt install -y \
   xorg bspwm kitty polybar suckless-tools rofi pass \
-  make unzip curl ufw lxappearance apt-listbugs \
+  make meson unzip curl ufw lxappearance apt-listbugs \
   alsa-utils pulseaudio libavcodec-extra qpdf \
   okular ranger feh syncthing arandr htop \
   neofetch jq neovim qt5-style-plugins firefox-esr \
   rsync materia-gtk-theme papirus-icon-theme
-
+  
+  # 
+  # picom
+  # 
+  
+  sudo apt install -y \
+  libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
+  libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev \
+  libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev \
+  libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev \
+  libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
+  
+  git clone https://github.com/yshui/picom
+  cd picom
+  git submodule update --init --recursive
+  meson --buildtype=release . build
+  ninja -C build install
+  
   # ------------------------------------------------------------------------------
   # vim-plug
   # https://github.com/junegunn/vim-plug
