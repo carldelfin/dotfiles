d2afc512721ca20561380a0588e50e7e46c5d856
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-17 15:44:46 +0100

    Create install_r.sh

---
+++ build/install_r.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ==============================================================================
+#
+# This script installs the latest version of R and Radian
+#
+# https://www.r-project.org/
+#
+# ==============================================================================
+
+set -e
+trap 'catch $? $LINENO' EXIT
+
+catch() {
+  if [ "$1" != "0" ]; then
+    echo -e "\033[1;31mInstallation failed!\033[0m"
+    echo -e "\033[1;31mError $1 occurred on $2\033[0m"
+  fi
+}
+
+simple() {
+  sudo apt install -y \
+  r-base r-base-dev \
+  libclang-dev libssl-dev libxml2-dev \
+  libcurl4-openssl-dev libssl-dev libxt-dev \
+  libopenblas-dev liblapack-dev libopencv-dev \
+  libcairo2-dev libnode-dev
+  
+  if ! command -v radian &> /dev/null; then
+      sudo apt install -y python3-pip
+      pip install -U radian
+      ln -s -f ~/dotfiles/config/.radian_profile ~/.radian_profile
+  else
+      echo "Radian is already installed"
+  fi
+
+}
+
+simple
+echo -e "\033[1;32mInstallation OK\033[0m"
