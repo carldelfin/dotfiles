1571788ff1be7c0e44f7afcccbf6c4dde7c51ac7
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-17 13:10:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,373 @@
 #!/bin/bash
 
-while test $# -gt 0; do
-  case "$1" in
-    -h|--help)
-      echo "$package - attempt to capture frames"
-      echo " "
-      echo "$package [options] application [arguments]"
-      echo " "
-      echo "options:"
-      echo "-h, --help                show help"
-      echo "-f, --full                do a full system install"
-      echo "-m, --minimal             do a minimal system install"
-      exit 0
-      ;;
-    -f|--full)
-      source ~/dotfiles/install/minimal_install.sh
-      source ~/dotfiles/install/full_install.sh
-      exit 0
-      ;;
-    -m|--minimal)
-      source ~/dotfiles/install/minimal_install.sh
-      exit 0
-      ;;
-    *)
-      break
-      ;;
-  esac
-done
+################################################################################
+#
+# This script gets my workstation up and running after a clean install.
+#
+# Do not use this script blindly, it might not do what you want! 
+# Make sure to read through this file and make changes accordingly.
+#
+################################################################################
+
+# ==============================================================================
+# Define a try catch function
+# Based on: https://medium.com/@dirk.avery/the-bash-trap-trap-ce6083f36700
+# ==============================================================================
+
+set -e
+trap 'catch $? $LINENO' EXIT
+
+catch() {
+  if [ "$1" != "0" ]; then
+    echo -e "\033[1;31mInstallation failed!\033[0m"
+    echo -e "\033[1;31mError $1 occurred on $2\033[0m"
+  fi
+}
+
+simple() {
+
+  # ==============================================================================
+  # Begin installation
+  # ==============================================================================
+
+  # ------------------------------------------------------------------------------
+  # Essential packages
+  # ------------------------------------------------------------------------------
+
+  sudo apt install -y \
+  bspwm kitty polybar suckless-tools rofi pass \
+  lxappearance libavcodec-extra qpdf inkscape\
+  okular ranger feh syncthing arandr htop \
+  neofetch jq neovim qt5-style-plugins
+  
+  # ------------------------------------------------------------------------------
+  # TexLive
+  # https://www.tug.org/texlive/
+  # ------------------------------------------------------------------------------
+  
+  sudo apt install -y texlive texlive-latex-extra
+  
+  # ------------------------------------------------------------------------------
+  # KVM
+  # https://www.linux-kvm.org/page/Main_Page
+  # ------------------------------------------------------------------------------
+  
+  sudo apt install -y \
+  qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+  
+  sudo adduser `id -un` libvirt
+  sudo adduser `id -un` kvm
+  
+  # ------------------------------------------------------------------------------
+  # vim-plug
+  # https://github.com/junegunn/vim-plug
+  # ------------------------------------------------------------------------------
+  
+  if [ -d "$HOME/.vim/plugged" ]; then
+      echo "vim-plug is already installed"
+  else
+      sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  fi
+  
+  # ------------------------------------------------------------------------------
+  # passmenu
+  # ------------------------------------------------------------------------------
+  
+  if ! command -v passmenu &> /dev/null; then
+      sudo cp /usr/share/doc/pass/examples/dmenu/passmenu /usr/bin/passmenu
+      sudo chmod +x /usr/bin/passmenu
+  else
+      echo "passmenu is already installed"
+  fi
+
+  # ------------------------------------------------------------------------------
+  # rofi-pass
+  # https://github.com/carnager/rofi-pass
+  # ------------------------------------------------------------------------------
+  
+  if ! command -v rofi-pass &> /dev/null; then
+      cd /tmp
+      wget https://github.com/carnager/rofi-pass/archive/master.zip
+      unzip master.zip
+      rm master.zip
+      cd rofi-pass-master
+      sudo make
+      mkdir -p ~/.config/rofi-pass
+      mv config.example ~/.config/rofi-pass/config
+      cd ..
+      sudo rm -rf rofi-pass-master
+      cd
+  else
+      echo "rofi-pass is already installed"
+  fi
+  
+  # ------------------------------------------------------------------------------
+  # rofi-power-menu
+  # https://github.com/jluttine/rofi-power-menu
+  # ------------------------------------------------------------------------------
+
+  if ! command -v rofi-power-menu &> /dev/null; then
+      cd /tmp
+      wget https://raw.githubusercontent.com/jluttine/rofi-power-menu/master/rofi-power-menu
+      sudo mv rofi-power-menu /usr/local/bin
+      sudo chmod +x /usr/local/bin/rofi-power-menu
+      cd
+  else
+      echo "rofi-power-menu is already installed"
+  fi
+ 
+  # ------------------------------------------------------------------------------
+  # rofi-bluetooth
+  # https://github.com/ClydeDroid/rofi-bluetooth
+  # ------------------------------------------------------------------------------
+
+  if ! command -v rofi-bluetooth &> /dev/null; then
+      cd /tmp
+      wget https://raw.githubusercontent.com/ClydeDroid/rofi-bluetooth/master/rofi-bluetooth
+      sudo mv rofi-bluetooth /usr/local/bin
+      sudo chmod +x /usr/local/bin/rofi-bluetooth
+      cd
+  else
+      echo "rofi-bluetooth is already installed"
+  fi
+  
+  # ------------------------------------------------------------------------------
+  # Mullvad
+  # https://mullvad.net
+  # ------------------------------------------------------------------------------
+  
+  if ! command -v mullvad &> /dev/null; then
+      cd /tmp
+      wget https://mullvad.net/media/app/MullvadVPN-2020.7_amd64.deb
+      sudo apt install -y ./*.deb
+      rm *.deb
+      cd
+  else
+      echo "Mullvad is already installed"
+  fi
+  
+  # ------------------------------------------------------------------------------
+  # R
+  # https://www.r-project.org/
+  # ------------------------------------------------------------------------------
+  
+  KEY=/usr/local/share/keyrings/marutter.key
+
+  if [ -f "$KEY" ]; then
+      echo "$KEY already exists"
+  else
+      curl -s 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xe298a3a825c0d65dfd57cbb651716619e084dab9' -o marutter.key
+      if ! file marutter.key | grep -q "PGP public key"; then
+          echo "marutter.key does not appear to be a valid PGP key - aborting!"
+          exit 1
+      else
+        sudo mkdir -p /usr/local/share/keyrings/
+        sudo mv marutter.key /usr/local/share/keyrings/
+        echo "deb [signed-by=/usr/local/share/keyrings/marutter.key] https://cloud.r-project.org/bin/linux/ubuntu groovy-cran40/" | sudo tee -a /etc/apt/sources.list
+      fi
+  fi
+
+  sudo apt update
+  
+  sudo apt install -y \
+  r-base r-base-dev \
+  libclang-dev libssl-dev \
+  libcurl4-openssl-dev libxt-dev \
+  libopenblas-dev liblapack-dev libopencv-dev \
+  libcairo2-dev libnode-dev
+  
+  # ------------------------------------------------------------------------------
+  # Radian
+  # https://github.com/randy3k/radian
+  # ------------------------------------------------------------------------------
+  
+  if ! command -v radian &> /dev/null; then
+      sudo apt install -y python3-pip
+      pip install -U radian
+      ln -s -f ~/dotfiles/config/.radian_profile ~/.radian_profile
+  else
+      echo "Radian is already installed"
+  fi
+  
+  # ------------------------------------------------------------------------------
+  # Signal
+  # https://signal.org
+  # ------------------------------------------------------------------------------
+  
+  if ! command -v signal-desktop &> /dev/null; then
+      KEY=/usr/local/share/keyrings/signal.key
+
+      if [ -f "$KEY" ]; then
+          echo "$KEY already exists"
+      else
+          curl -s 'https://updates.signal.org/desktop/apt/keys.asc' -o signal.key
+          if ! file signal.key | grep -q "PGP public key"; then
+              echo "signal.key does not appear to be a valid PGP key - aborting!"
+              exit 1
+          else
+              sudo mkdir -p /usr/local/share/keyrings/
+              sudo mv signal.key /usr/local/share/keyrings/
+              echo "deb [signed-by=/usr/local/share/keyrings/signal.key] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+          fi
+      fi
+      sudo apt update
+      sudo apt install -y signal-desktop
+  else
+      echo "Signal is already installed"
+  fi
+  
+  # ------------------------------------------------------------------------------
+  # Zoom
+  # https://zoom.us/
+  # ------------------------------------------------------------------------------
+  
+  if ! command -v zoom &> /dev/null; then
+      cd /tmp
+      wget https://zoom.us/client/latest/zoom_amd64.deb
+      sudo apt install -y ./*.deb
+      rm *.deb
+      cd
+  else
+      echo "zoom is already installed"
+  fi
+  
+  # ------------------------------------------------------------------------------
+  # Zotero
+  # https://www.zotero.org/
+  # ------------------------------------------------------------------------------
+  
+  if ! command -v zotero &> /dev/null; then
+      KEY=/usr/local/share/keyrings/zotero.key
+
+      if [ -f "$KEY" ]; then
+          echo "$KEY already exists"
+      else
+          curl -s "https://github.com/retorquere/zotero-deb/releases/download/apt-get/deb.gpg.key" -o zotero.key
+          if ! file zotero.key | grep -q "PGP public key"; then
+              echo "zotero.key does not appear to be a valid PGP key - aborting!"
+              exit 1
+          else
+              sudo mkdir -p /usr/local/share/keyrings/
+              sudo mv zotero.key /usr/local/share/keyrings/
+              echo "deb [signed-by=/usr/local/share/keyrings/zotero.key] deb https://github.com/retorquere/zotero-deb/releases/download/apt-get/ ./" | sudo tee -a /etc/apt/sources.list.d/zotero.list
+          fi
+      fi
+      sudo apt update
+      sudo apt install -y zotero
+  else
+      echo "Zotero is already installed"
+  fi
+
+  
+  # ==============================================================================
+  # Remove redundant packages 
+  # ==============================================================================
+  
+  sudo apt remove -y --allow-remove-essential \
+  gnome-terminal gnome-calendar gedit geary eog evince totem
+  
+  # ==============================================================================
+  # Install fonts 
+  # ==============================================================================
+  
+  # ------------------------------------------------------------------------------
+  # Fira Code
+  # https://github.com/tonsky/FiraCode
+  # ------------------------------------------------------------------------------
+  
+  if fc-list | grep -q FiraCode; then
+    echo "Fira Code is already installed"
+  else
+    mkdir -p ~/.local/share/fonts
+    cd ~/.local/share/fonts
+    wget https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip
+    unzip *.zip
+    mv ~/.local/share/fonts/ttf/*.ttf ~/.local/share/fonts
+    rm *.txt
+    rm *.html
+    rm *.zip
+    rm *.css
+    rm -rf woff
+    rm -rf woff2
+    rm -rf variable_ttf
+    rm -rf ttf
+  fi
+
+  # ------------------------------------------------------------------------------
+  # Font Awesome
+  # https://fontawesome.com/
+  # ------------------------------------------------------------------------------
+  
+  if fc-list | grep -q "Font Awesome"; then
+    echo "Font Awesome is already installed"
+  else
+    mkdir -p ~/.local/share/fonts
+    wget https://use.fontawesome.com/releases/v5.15.2/fontawesome-free-5.15.2-desktop.zip
+    unzip *.zip
+    mv fontawesome-free-5.15.2-desktop/otfs/*.otf ~/.local/share/fonts/
+    rm *.zip
+    rm -rf fontawesome-free-5.15.2-desktop
+    cd
+  fi
+ 
+  fc-cache -f
+  
+  # ==============================================================================
+  # Make sure relevant configs and scripts are executable
+  # ==============================================================================
+  
+  sudo chmod +x ~/dotfiles/config/bspwm/bspwmrc
+  sudo chmod +x ~/dotfiles/scripts/launch.sh
+  sudo chmod +x ~/dotfiles/scripts/vpn.sh
+  sudo chmod +x ~/dotfiles/scripts/sync.sh
+  sudo chmod +x ~/dotfiles/scripts/upgrades.sh
+  sudo chmod +x ~/dotfiles/scripts/backup.sh
+  sudo chmod +x ~/dotfiles/scripts/weather.R
+
+  # ==============================================================================
+  # Set up symlinks and paths
+  # ==============================================================================
+  
+  mkdir -p ~/.config/{bspwm,sxhkd,kitty,rofi,rofi-pass,ranger,nvim}
+  ranger --copy-config=all
+
+  ln -s -f ~/dotfiles/config/.bashrc ~/.bashrc
+  ln -s -f ~/dotfiles/config/mimeapps.list ~/.config/mimeapps.list
+  ln -s -f ~/dotfiles/config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+  ln -s -f ~/dotfiles/config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+  ln -s -f ~/dotfiles/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+  ln -s -f ~/dotfiles/config/rofi/my_theme.rasi ~/.config/rofi/my_theme.rasi
+  ln -s -f ~/dotfiles/config/rofi-pass/config ~/.config/rofi-pass/config
+  ln -s -f ~/dotfiles/config/ranger/rifle.conf ~/.config/ranger/rifle.conf
+  ln -s -f ~/dotfiles/config/ranger/rc.conf ~/.config/ranger/rc.conf
+  ln -s -f ~/dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
+  
+  if ! cat /etc/environment | grep -q "QT_QPA_PLATFORMTHEME=gtk2"; then
+    echo "QT_QPA_PLATFORMTHEME=gtk2" | sudo tee -a /etc/environment
+  fi
+
+  # ==============================================================================
+  # Enable UFW
+  # ==============================================================================
+  
+  sudo ufw default deny incoming
+  sudo ufw default allow outgoing
+  sudo ufw allow from 192.168.20.0/24
+  sudo ufw enable
+  sudo ufw allow syncthing
+  
+  # ==============================================================================
+  # Clean up and upgrade
+  # ==============================================================================
+  
+  sudo apt -y autoremove
+  sudo apt update && sudo apt -y upgrade
+ 
+}
+
+simple
+echo ""
+echo -e "\033[1;32mInstall OK\033[0m"
+echo ""
+
