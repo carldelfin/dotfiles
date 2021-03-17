62efb3391e31ac85bda6702374411796b719e2bd
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-17 19:47:06 +0100

    Update 02_system_setup.sh

--- build/02_system_setup.sh
+++ build/02_system_setup.sh
@@ -34,18 +34,20 @@ simple() {
   # picom
   # ------------------------------------------------------------------------------
   
-  sudo apt install -y \
-  libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
-  libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev \
-  libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev \
-  libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev \
-  libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
+  sudo apt install -y picom
   
-  git clone https://github.com/yshui/picom
-  cd picom
-  git submodule update --init --recursive
-  meson --buildtype=release . build
-  sudo ninja -C build install
+  #sudo apt install -y \
+  #libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
+  #libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev \
+  #libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev \
+  #libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev \
+  #libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
+  
+  #git clone https://github.com/yshui/picom
+  #cd picom
+  #git submodule update --init --recursive
+  #meson --buildtype=release . build
+  #sudo ninja -C build install
   
   # ------------------------------------------------------------------------------
   # passmenu
@@ -145,7 +147,7 @@ simple() {
   # Sound
   # ------------------------------------------------------------------------------
   
-  sudo apt install -y alsa-utils pulseaudio libavcodec-extra 
+  sudo apt install -y pulseaudio alsa-utils libavcodec-extra 
   
   # ------------------------------------------------------------------------------
   # vim-plug
