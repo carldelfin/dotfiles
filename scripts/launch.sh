124898a745da0ce294d13f3d149d327f453fe895
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-18 16:54:07 +0100

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -21,7 +21,7 @@ polybar -c ~/dotfiles/config/polybar/config cmd &
 picom --config ~/dotfiles/config/picom/picom.conf
 
 # set desktop background
-hsetroot -solid "#161616"
+hsetroot -solid "#1E1E1E"
 
 # enable sound
 pulseaudio --start
