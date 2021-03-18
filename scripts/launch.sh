3246ba1b048b407a430c5047b261b70488451132
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-18 15:22:01 +0100

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,9 +6,28 @@ killall -q polybar
 # wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# which hwmon directory contains k10temp?
-k10temp_dir=$(grep -lr "k10temp" /sys/class/hwmon/* -s | sed 's,/*[^/]\+/*$,,')
-export HWMON_DIR=$k10temp_dir/temp3_input # CCD1
+# which hwmon directory contains CPU temp?
+if cat /proc/cpuinfo | grep "vendor" | uniq | grep -q "AMD"; then
+  k10temp_dir=$(grep -lr "k10temp" /sys/class/hwmon/* -s | sed 's,/*[^/]\+/*$,,')
+  export HWMON_DIR=$k10temp_dir/temp3_input # CCD1
+else
+  export HWMON_DIR="/sys/class/hwmon/hwmon0/temp1_input"# CCD1
+fi
 
 # launch polybar
 polybar -c ~/dotfiles/config/polybar/config cmd &
+
+# launch picom
+picom --config ~/dotfiles/config/picom/picom.conf
+
+# set wallpaper
+feh --bg-scale ~/dotfiles/img/wallpaper.png
+
+# enable sound
+pulseaudio --start
+
+# start syncthing
+syncthing -no-browser
+
+# set correct mouse cursor
+xsetroot -cursor_name left_ptr
