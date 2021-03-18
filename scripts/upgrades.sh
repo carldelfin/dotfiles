a8ff3f478222b292efec484c888198d7b4fa3c2d
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-18 15:57:41 +0100

    Update upgrades.sh

--- scripts/upgrades.sh
+++ scripts/upgrades.sh
@@ -3,7 +3,7 @@
 num_upgrades=$(apt-get -s dist-upgrade | grep -Po "^[[:digit:]]+ (?=upgraded)" | sed -e 's/[[:space:]]*$//')
 
 if (( $num_upgrades > 0 )); then
-    echo "%{F#FBB86C}$num_upgrades%{F-}"
+    echo "%{F#A0520B}$num_upgrades%{F-}"
 else
     echo "%{F#D2D2D2}$num_upgrades%{F-}"
 fi
