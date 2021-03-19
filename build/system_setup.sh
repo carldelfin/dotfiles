b23872e2b3f28728b619f1573675a55d55b54f82
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-19 10:03:56 +0100

    Update system_setup.sh

--- build/system_setup.sh
+++ build/system_setup.sh
@@ -206,11 +206,13 @@ simple() {
   # Set up symlinks and paths
   # ==================================================================================================
   
-  mkdir -p ~/.config/{bspwm,sxhkd,kitty,rofi,rofi-pass,ranger,nvim}
+  mkdir -p ~/.config/{bspwm,sxhkd,kitty,rofi,rofi-pass,ranger,nvim,gtk-3.0}
   ranger --copy-config=all
 
   ln -s -f ~/dotfiles/config/.bashrc ~/.bashrc
   ln -s -f ~/dotfiles/config/.xinitrc ~/.xinitrc
+  ln -s -f ~/dotfiles/config/gtk/.gtkrc-2.0 ~/.config/.gtkrc-2.0
+  ln -s -f ~/dotfiles/config/gtk/settings.ini ~/.config/gtk-3.0/settings.ini
   ln -s -f ~/dotfiles/config/mimeapps.list ~/.config/mimeapps.list
   ln -s -f ~/dotfiles/config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
   ln -s -f ~/dotfiles/config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
