2c97993541132e5c41f968789700478ffdbe6d4f
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-19 12:52:34 +0100

    Update system_setup.sh

--- build/system_setup.sh
+++ build/system_setup.sh
@@ -259,7 +259,7 @@ simple() {
   
   sudo ufw default deny incoming
   sudo ufw default allow outgoing
-  sudo ufw allow from 192.168.20.0/24
+  sudo ufw allow from 192.168.20.0/24 to any port 22 # allow ssh connections from within LAN
   sudo ufw enable
   sudo ufw allow syncthing
  
