4549e32184d69f8921255bbb4597432b6a54722c
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-03-19 08:33:42 +0100

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # terminate already running bar instances
-pkill -q polybar
+killall -q polybar
 
 # wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
@@ -11,17 +11,17 @@ if cat /proc/cpuinfo | grep "vendor" | uniq | grep -q "AMD"; then
   k10temp_dir=$(grep -lr "k10temp" /sys/class/hwmon/* -s | sed 's,/*[^/]\+/*$,,')
   export HWMON_DIR=$k10temp_dir/temp3_input # CCD1
 else
-  export HWMON_DIR="/sys/class/hwmon/hwmon0/temp1_input"
+  export HWMON_DIR="/sys/class/hwmon/hwmon0/temp1_input"# CCD1
 fi
 
 # launch polybar
-polybar -c ~/dotfiles/config/polybar/config cmd &
+polybar -c ~/dotfiles/config/polybar/polybar.conf cmd &
 
 # launch picom
 picom --config ~/dotfiles/config/picom/picom.conf &
 
 # set desktop background
-hsetroot -solid "#1b1d22"
+hsetroot -solid "#1B1D22"
 
 # enable sound
 pulseaudio --start
