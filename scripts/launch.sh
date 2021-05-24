98472e5a42decbf55dab8d35c242b14d11808591
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-05-24 16:13:25 +0200

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -30,7 +30,7 @@ while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 picom --config ~/dotfiles/config/picom/picom.conf &
 
 # set desktop background
-feh --bg-scale --randomize ~/dotfiles/wallpapers/* &
+feh --bg-scale ~/dotfiles/wallpapers/pop_nord.png &
 
 # start syncthing
 syncthing -no-browser
