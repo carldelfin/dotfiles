9a544ace58648ba5e42e54df6971360a7c9d4800
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-05-25 07:50:58 +0200

    Update system_setup.sh

--- build/system_setup.sh
+++ build/system_setup.sh
@@ -108,9 +108,7 @@ simple() {
       cd /tmp
       curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
       chmod u+x nvim.appimage
-      ./nvim.appimage --appimage-extract
-      sudo mv squashfs-root / && sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-      rm nvim.appimage
+      sudo mv nvim.appimage /usr/bin/
       cd
       
       pip3 install pynvim
