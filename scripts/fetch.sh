da353928874f8308f0b7422ba10bde4929796e7f
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-05-26 21:54:13 +0200

    Automated dotfiles commit + push via terminal

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-OS=$(hostnamectl | grep "Operating" | awk '{print $3}')
-OSVER=$(hostnamectl | grep "Operating" | awk '{print $5}')
-TERMINAL=$(echo $TERM | grep -oP '(?<=xterm-).*')
-SH=$(echo $SHELL | sed 's:.*/::')
+user="$(whoami)"
+host="$(hostname)"
+os="$(lsb_release -ds)"
+kernel="$(uname -r | sed 's/-.*//')"
+terminal=$(echo $TERM | grep -oP '(?<=xterm-).*')
+uptime="$(uptime -p | sed 's/up //')"
+packages="$(dpkg -l | wc -l)"
+shell=$(echo $SHELL | sed 's:.*/::')
+ram_used=$(free -m | grep "Mem:" | awk {'print $3'}) 
+ram_avail=$(free -m | grep "Mem:" | awk {'print $2'})
+cpu="$(lscpu | grep -oP "Model name:\K.*" | awk {'print $2,$3,$4'})"
+
+xpropid="$(xprop -root -notype | \awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}')"
+wm="$(xprop -id "$xpropid" -notype -f _NET_WM_NAME 8t)"
+wm=${wm/*_NET_WM_NAME = }
+wm=${wm/\"}
+wm=${wm/\"*}
+
 
 RED='\033[0;31m'
 YLW='\033[1;33m'
@@ -13,13 +27,13 @@ NC='\033[0m' # No Color
 
 clear
 
-echo -e "${CYN}${NC} ${YLW}usr${NC}         $(whoami)"
-echo -e " host        $(hostnamectl | grep "Static" | awk {'print $3'})"
-echo -e " os          $OS $OSVER"
-echo -e " kern        $(hostnamectl | grep "Kernel" | awk {'print $3'})"
-echo -e " wm          $(xprop -id $(xprop -root -notype | awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}') | grep "NET_WM_NAME" | cut -d '"' -f2)"
-echo -e " sh          $SH"
-echo -e " term        $TERMINAL"
-echo -e " ed          $EDITOR"
-echo -e " cpu         $(lscpu | grep -oP "Model name:\K.*" | sed -e 's/^[ \t]*//')"
-echo -e " ram         $(free -m | grep "Mem:" | awk {'print $3'}) MiB / $(free -m | grep "Mem:" | awk {'print $2'}) MiB"
+echo -e "${YLW}user${NC}         $user"
+echo -e "${YLW}host${NC}         $host"
+echo -e "${YLW}os${NC}           $os"
+echo -e "${YLW}kernel${NC}       $kernel"
+echo -e "${YLW}wm${NC}           $wm"
+echo -e "${YLW}terminal${NC}     $terminal"
+echo -e "${YLW}shell${NC}        $shell"
+echo -e "${YLW}packages${NC}       $packages"
+echo -e "${YLW}memory${NC}       ${ram_used}M / ${ram_avail}M"
+echo -e "${YLW}cpu${NC}          $cpu"
