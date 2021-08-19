5cdcb4c893256df183abe78898d66e4c77a196d7
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-08-19 13:20:18 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -38,7 +38,88 @@ simple() {
   software-properties-common apt-transport-https build-essential \
   ca-certificates dirmngr make cmake gcc libgtk-3-dev apt-listbugs \
   zathura nautilus feh unzip htop syncthing ufw rsync neofetch firefox \
-  alsa-utils pulseaudio libavcodec-extra 
+  alsa-utils pulseaudio libavcodec-extra \
+  python3-pip exfat-fuse exfat-utils
+  
+  # passmenu  
+  if ! command -v passmenu &> /dev/null; then
+      sudo cp /usr/share/doc/pass/examples/dmenu/passmenu /usr/bin/passmenu
+      sudo chmod +x /usr/bin/passmenu
+  else
+      echo "passmenu is already installed"
+  fi
+
+  # rofi-pass
+  if ! command -v rofi-pass &> /dev/null; then
+      cd /tmp
+      wget https://github.com/carnager/rofi-pass/archive/master.zip
+      unzip master.zip
+      rm master.zip
+      cd rofi-pass-master
+      sudo make
+      mkdir -p ~/.config/rofi-pass
+      mv config.example ~/.config/rofi-pass/config
+      cd ..
+      sudo rm -rf rofi-pass-master
+      cd
+  else
+      echo "rofi-pass is already installed"
+  fi
+  
+  # rofi-power-menu
+  if ! command -v rofi-power-menu &> /dev/null; then
+      cd /tmp
+      wget https://raw.githubusercontent.com/jluttine/rofi-power-menu/master/rofi-power-menu
+      sudo mv rofi-power-menu /usr/local/bin
+      sudo chmod +x /usr/local/bin/rofi-power-menu
+      cd
+  else
+      echo "rofi-power-menu is already installed"
+  fi
+   
+  # rofi-bluetooth (own fork using my custom theme)
+  if ! command -v rofi-bluetooth &> /dev/null; then
+      cd /tmp
+      wget https://raw.githubusercontent.com/carldelfin/rofi-bluetooth/master/rofi-bluetooth
+      sudo mv rofi-bluetooth /usr/local/bin
+      sudo chmod +x /usr/local/bin/rofi-bluetooth
+      cd
+  else
+      echo "rofi-bluetooth is already installed"
+  fi
+  
+  # Mullvad
+  #if ! command -v mullvad &> /dev/null; then
+  #    cd /tmp
+  #    wget https://mullvad.net/media/app/MullvadVPN-2021.3_amd64.deb
+  #    sudo apt install -y ./*.deb
+  #    rm *.deb
+  #    cd
+  #else
+  #    echo "Mullvad is already installed"
+  #fi
+
+  # neovim
+  if ! command -v nvim &> /dev/null; then
+      cd /tmp
+      curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+      chmod u+x nvim.appimage
+      sudo mv nvim.appimage /usr/bin/
+      cd
+      
+      pip3 install pynvim
+
+  else
+      echo "Neovim is already installed"
+  fi
+
+  # vim-plug
+  if [ -d "$HOME/.vim/plugged" ]; then
+      echo "vim-plug is already installed"
+  else
+      sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  fi
 }
 
 simple
