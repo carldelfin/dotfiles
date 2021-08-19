10f94dac5b63ce81772da4c19b71199263929c63
carldelfin <22149864+carldelfin@users.noreply.github.com>
2021-08-19 13:37:38 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -32,14 +32,14 @@ simple() {
   # Install required packages
   # ------------------------------------------------------------------------------
 
-  sudo apt install -y \
+  #sudo apt install -y \
   #xorg x11-xserver-utils \
   #bspwm sxhkd picom kitty polybar suckless-tools rofi pass arandr \
   #software-properties-common apt-transport-https build-essential \
   #ca-certificates dirmngr make cmake gcc libgtk-3-dev apt-listbugs \
   #zathura nautilus feh unzip htop syncthing ufw rsync neofetch firefox \
   #alsa-utils pulseaudio libavcodec-extra \
-  curl python3-pip exfat-fuse exfat-utils
+  #curl python3-pip exfat-fuse exfat-utils
 
   
   # passmenu  
@@ -121,6 +121,74 @@ simple() {
       sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   fi
+  
+  # --------------------------------------------------------------------------------------------------
+  # Appearance
+  # --------------------------------------------------------------------------------------------------
+  
+  echo ""
+  echo -e "\033[1;33mInstalling themes and fonts...\033[0m"
+  echo ""
+
+  # Tokyo Night GTK
+  sudo mkdir -p /usr/share/themes
+  cd /tmp
+  wget https://github.com/koiosdev/Tokyo-Night-Linux/archive/refs/heads/master.zip
+  unzip master.zip
+  cd Tokyo-Night-Linux-master/usr/share/themes
+  sudo cp -r TokyoNight /usr/share/themes/
+  cd /tmp
+  rm -rf Tokyo-Night-Linux-master 
+  rm master.zip
+  cd
+
+  # Numix icons
+  sudo apt-get install numix-icon-theme
+  
+  # Pop icons
+  sudo apt-add-repository ppa:system76/pop
+  sudo apt update
+  sudo apt install pop-fonts
+  
+  # JetBrainsMono with Nerd Font patch
+  if fc-list | grep -q JetBrains; then
+      echo "JetBrainsMono is already installed"
+  else
+      mkdir -p ~/.local/share/fonts
+      cd ~/.local/share/fonts
+      wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+      unzip *.zip
+      rm *.zip
+  fi
+
+  # Font Awesome
+  if fc-list | grep -q "Font Awesome"; then
+      echo "Font Awesome is already installed"
+  else
+      mkdir -p ~/.local/share/fonts
+      cd /tmp
+      wget https://use.fontawesome.com/releases/v5.15.2/fontawesome-free-5.15.2-desktop.zip
+      unzip *.zip
+      mv fontawesome-free-5.15.2-desktop/otfs/*.otf ~/.local/share/fonts/
+      rm *.zip
+      rm -rf fontawesome-free-5.15.2-desktop
+      cd
+  fi
+  
+  # Google fonts
+  if fc-list | grep -q "Roboto"; then
+      echo "Google fonts are already installed"
+  else
+      mkdir -p ~/.local/share/fonts
+      cd /tmp
+      wget -O fonts.zip "https://fonts.google.com/download?family=Roboto|Noto%20Sans|Open%20Sans|Roboto%20Condensed|Source%20Sans%20Pro|Raleway|Merriweather|Roboto%20Slab|PT%20Sans|Open%20Sans%20Condensed|Droid%20Sans|Droid%20Serif|Fira%20Sans|Fira%20Sans%20Condensed|Fira%20Sans%20Extra%20Condensed|Fira%20Mono"
+      unzip fonts.zip -d ~/.local/share/fonts
+      rm -rf fonts.zip
+      cd
+  fi
+  
+  fc-cache -f
+  
 }
 
 simple
