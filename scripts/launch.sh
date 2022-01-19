65ba06e0f84999836aa182a2554067d1660ffab9
carldelfin <22149864+carldelfin@users.noreply.github.com>
2022-01-19 21:58:39 +0100

    Automated dotfiles commit + push from terminal

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -30,7 +30,7 @@ while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 picom --config ~/dotfiles/config/picom/picom.conf &
 
 # set desktop background
-feh --bg-scale ~/dotfiles/wallpaper/stairs.jpg &
+feh --bg-scale ~/dotfiles/wallpaper/greenery.jpg &
 
 # start syncthing
 syncthing -no-browser
