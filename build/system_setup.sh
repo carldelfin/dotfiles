043c3f22769b869e5ebe30388e641bdeeec94305
Carl Delfin <22149864+carldelfin@users.noreply.github.com>
2022-02-14 17:38:11 +0100

    Update system_setup.sh

--- build/system_setup.sh
+++ build/system_setup.sh
@@ -26,7 +26,7 @@ simple() {
 
   # go
   cd /tmp
-  wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
+  wget https://golang.org/dl/go1.17.7.linux-amd64.tar.gz
   sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
   rm go1.17.linux-amd64.tar.gz
   cd
