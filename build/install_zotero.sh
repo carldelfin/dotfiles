7bcb81fd9ad41b102d3afb98127169bf07d36980
Carl Delfin <22149864+carldelfin@users.noreply.github.com>
2022-02-15 08:50:41 +0100

    Update install_zotero.sh

--- build/install_zotero.sh
+++ build/install_zotero.sh
@@ -20,23 +20,9 @@ catch() {
 
 simple() {
   if ! command -v zotero &> /dev/null; then
-      KEY=/usr/local/share/keyrings/zotero.key
-
-      if [ -f "$KEY" ]; then
-          echo "$KEY already exists"
-      else
-          wget -q -O zotero.key "https://github.com/retorquere/zotero-deb/releases/download/apt-get/deb.gpg.key"
-          if ! file zotero.key | grep -q "PGP public key"; then
-              echo "zotero.key does not appear to be a valid PGP key - aborting!"
-              exit 1
-          else
-              sudo mkdir -p /usr/local/share/keyrings/
-              sudo mv zotero.key /usr/local/share/keyrings/
-              echo "deb [signed-by=/usr/local/share/keyrings/zotero.key] https://github.com/retorquere/zotero-deb/releases/download/apt-get/ ./" | sudo tee -a /etc/apt/sources.list.d/zotero.list
-          fi
-      fi
+      wget -qO- https://apt.retorque.re/file/zotero-apt/install.sh | sudo bash
       sudo apt update
-      sudo apt install -y zotero default-jre
+      sudo apt install zotero
   else
       echo "Zotero is already installed"
   fi
