fa25191bdb3691d5896e33b0749bd107d37a6482
carldelfin <22149864+carldelfin@users.noreply.github.com>
2022-02-17 11:10:16 +0100

    Automated dotfiles commit + push from terminal

--- build/system_setup.sh
+++ build/system_setup.sh
@@ -273,7 +273,7 @@ simple() {
   wget https://github.com/valentjn/ltex-ls/releases/download/15.2.0/ltex-ls-15.2.0-linux-x64.tar.gz
   mkdir -p ~/.local/bin
   tar -C ~/.local/bin -xzf ltex-ls-15.2.0-linux-x64.tar.gz
-  export PATH=$PATH:/bin/ltex-ls-15.2.0-linux-x64/bin
+  export PATH=$PATH:/.local/bin/ltex-ls-15.2.0-linux-x64/bin
 
 
   # ------------------------------------------------------------------------------------------------
